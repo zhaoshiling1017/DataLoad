755ed3a801a4c864f972a28642200d4017b98735
zhaosl <zhaosl1017@gmail.com>
2014-10-28 21:13:35 +0800

    modify start.sh

--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@
 cd /apps/DataLoad/output/
 JAVA_PATH=`which java`
 JAR_NAME=DataLoad.jar 
-$JAVA_PATH -jar $JAR_NAME 192.168.1.229 root root 192.168.1.231 postgres postgres em_1147061 /tmp/ root bjiamcall
+$JAVA_PATH -jar $JAR_NAME 127.0.0.1 root root 127.0.0.1 postgres postgres em_1147418 /tmp root bjiamcall
