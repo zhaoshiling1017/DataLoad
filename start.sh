4800010ab110593091764308a457b475b036eb31
zhaosl <zhaosl1017@gmail.com>
2014-11-06 10:56:54 +0800

    ddd

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd /apps/DataLoad/output/
 JAVA_PATH=`which java`
-JAR_NAME=DataLoad.jar 
+JAR_NAME=DataLoad.jar
 $JAVA_PATH -jar $JAR_NAME 192.168.1.231 root root 127.0.0.1 postgres postgres em_7851 /tmp root bjiamcall
